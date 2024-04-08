9d06e19635602a6eb55c192ce2bdbd9e617483a2
Alícia Antunes <antunesalicia07@gmail.com>
2024-04-08 18:40:06 +0100

    change app and index

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getQuestionsHandler } from './redux/action-handlers/questionsActions';
 
 function App() {
+  const dispatch = useDispatch();
+  const questions = useSelector(state => state.questions);
+
+  const fetchQuestions = () => {
+    console.log('Fetching questions...');
+    dispatch(getQuestionsHandler());
+  };
+
+
+  console.log('questions:', questions);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button onClick={fetchQuestions}>Fetch Questions</button>
+        <ul>
+          {questions.map(question => (
+            <li key={question.id}>{question.optionOne.text}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
