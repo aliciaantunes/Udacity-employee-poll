971b7bf616cae423092c0997e3a81143bdc5e658
Alícia Antunes <antunesalicia07@gmail.com>
2024-04-10 13:47:38 +0100

    feat: create login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,60 @@
-import React, { useEffect } from 'react';
-import { useLocation } from 'wouter';
+import React, { useState } from 'react';
 
-function Login() {
-  const [location, setLocation] = useLocation();
-  const isUserAuthenticated = () => !!localStorage.getItem('user');
+function LoginForm() {
+  const [username, setUsername] = useState('tylermcginnis');
+  const [password, setPassword] = useState('abc321');
 
-  useEffect(() => {
-    if (!isUserAuthenticated()) {
-      // Redirects to login page if user is not authenticated
-      setLocation('/login');
-    }
-  }, [location]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
 
   return (
-    <div>
-      <h1>Login</h1>
-      {/* Your component JSX goes here */}
+    <div className="max-w-lg mx-auto mt-5 bg-white shadow-lg rounded-lg overflow-hidden">
+      <form
+        onSubmit={handleSubmit}
+        className="px-8 py-6"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username:
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password:
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="flex items-center justify-between">
+          <input
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+            value="Submit"
+          />
+        </div>
+      </form>
     </div>
   );
 }
-export default Login;
+
+export default LoginForm;
